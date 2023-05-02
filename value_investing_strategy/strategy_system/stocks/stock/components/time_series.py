2cb183db2c5a7896937b74ff65f3fc0a4dd74a3e
Jamie Mortensen <16mortensenj@gmail.com>
2023-05-02 19:05:22 -0400

    time series changes

--- value_investing_strategy/strategy_system/stocks/stock/components/time_series.py
+++ value_investing_strategy/strategy_system/stocks/stock/components/time_series.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from value_investing_strategy.strategy_system.stocks.stock.components.stock_component import StockComponent
 
+from datetime import datetime
 import pandas as pd
 from dataclasses import dataclass
 from typing import Any, Optional
@@ -8,7 +9,7 @@ from typing import Any, Optional
 
 @dataclass
 class MonthlyData:
-    date: str
+    date: datetime
     open: float
     high: float
     low: float
@@ -19,7 +20,7 @@ class MonthlyData:
 @dataclass
 class MetaData:
     symbol: str
-    last_refreshed: str
+    last_refreshed: datetime
     time_zone: str
 
 
@@ -33,13 +34,14 @@ class TimeSeriesMonthly(StockComponent):
         meta_data_dict = data.get("Meta Data", {})
         meta_data = MetaData(
             symbol=meta_data_dict.get("2. Symbol", ""),
-            last_refreshed=meta_data_dict.get("3. Last Refreshed", ""),
+            last_refreshed=pd.to_datetime(
+                meta_data_dict.get("3. Last Refreshed", "")),
             time_zone=meta_data_dict.get("4. Time Zone", ""),
         )
         raw_monthly_time_series = data.get("Monthly Time Series", {})
         monthly_time_series = [
             MonthlyData(
-                date=date,
+                date=pd.to_datetime(date),
                 open=float(info.get("1. open", 0)),
                 high=float(info.get("2. high", 0)),
                 low=float(info.get("3. low", 0)),
@@ -55,16 +57,14 @@ class TimeSeriesMonthly(StockComponent):
         data = cls.load_json_dict(path)
         return cls.from_dict(data)
 
-    def find_nearest_data(self, target_date: str) -> Optional[MonthlyData]:
-        target_date = pd.to_datetime(target_date)
-        dates = [pd.to_datetime(data.date)
-                 for data in self.monthly_time_series]
+    def find_nearest_data(self, target_date: datetime) -> Optional[MonthlyData]:
+        dates = [data.date for data in self.monthly_time_series]
         nearest_date = min(dates, key=lambda x: abs(x - target_date))
-        nearest_data = next((data for data in self.monthly_time_series if pd.to_datetime(
-            data.date) == nearest_date), None)
+        nearest_data = next(
+            (data for data in self.monthly_time_series if data.date == nearest_date), None)
         return nearest_data
 
-    def calculate_stock_returns(self, initial_date: str, final_date: str) -> Optional[float]:
+    def calculate_stock_returns(self, initial_date: datetime, final_date: datetime) -> Optional[float]:
         initial_data = self.find_nearest_data(initial_date)
         final_data = self.find_nearest_data(final_date)
 
